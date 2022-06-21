cce78c8eeb6a5acf4381c502669dd89672c00633
Michael Alexeev <alexeev.michael.1990@gmail.com>
2022-06-21 23:21:20 +0400

    add vuejs2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import Vue from 'vue';
+import App from "./components/App.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.querySelector('#app');
+
+    if (app) {
+        new Vue({
+            el: app,
+            components: {
+                App,
+            },
+            template: '<app/>'
+        });
+    }
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Hello, App!</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        }
+    }
+</script>
+
+<style lang="scss">
+div {
+    color: green;
+}
+</style>
